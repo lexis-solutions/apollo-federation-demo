53e76ac60b8e2262d3ec9a8a7687c2201c9ffde6
Bilyal Mestanov <bmestanov@gmail.com>
2021-10-24 17:05:57 +0300

    Replace apollo-server with apollo-server-express

--- packages/gateway/src/index.ts
+++ packages/gateway/src/index.ts
@@ -1,6 +1,7 @@
 import { ApolloGateway } from "@apollo/gateway";
-import { ApolloServer } from "apollo-server";
+import { ApolloServer } from "apollo-server-express";
 import waitOn from "wait-on";
+import express from "express";
 
 export const serviceList = [
   {
@@ -28,13 +29,24 @@ async function buildApp() {
 
 const port = process.env.PORT || 4000;
 
-console.info("waiting for services to come online...");
+const app = express();
 
-waitOn({
-  resources: serviceList.map((service) => service.url),
-  validateStatus: () => true,
-  timeout: 60 * 1000,
-})
-  .then(buildApp)
-  .then((server) => server.listen(port))
-  .then(({ url }) => console.info(`Gateway server listening on ${url}.`));
+app.listen(port, () => {
+  console.info(`started gateway server on http://localhost:${port}`);
+  console.info("waiting for services to come online...");
+  waitOn({
+    resources: serviceList.map((service) => service.url),
+    validateStatus: () => true,
+    timeout: 60 * 1000,
+  })
+    .then(buildApp)
+    .then(async (server) => {
+      await server.start();
+
+      server.applyMiddleware({
+        app,
+        path: "/",
+      });
+    })
+    .then(() => console.info(`gateway set up successfully`));
+});
